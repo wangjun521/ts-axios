2844a8dea72d3b0669047c0ae398d98422c9e6fe
wangj <wangjun20101120@163.com>
2019-07-24 16:30:07 +0800

    部分错误返回处理

---
+++ src/helpers/headers.ts
@@ -0,0 +1,45 @@
+import { isPlainObject } from './util'
+
+function normalizeHeaderName(headers: any, normalizeName: string): void {
+  if (!headers) {
+    return
+  }
+  Object.keys(headers).forEach(name => {
+    if (name !== normalizeName && name.toUpperCase() === normalizeName.toUpperCase()) {
+      headers[normalizeName] = headers[name]
+      delete headers[name]
+    }
+  })
+}
+
+export function processHeaders(headers: any, data:any): any {
+  normalizeHeaderName(headers, 'Content-type')
+
+  if (isPlainObject(data)) {
+    if (headers && !headers['Content-type']) {
+      headers['Content-type'] = 'application/json;charset=utf-8'
+    }
+  }
+  return headers
+}
+
+export function parseHeaders(headers: string): any {
+  let parsed = Object.create(null)
+  if (!headers) {
+    return parsed
+  }
+
+  headers.split('\r\n').forEach((line) => {
+    let [key, val] = line.split(':')
+    key = key.trim().toLowerCase()
+    if (!key) {
+      return
+    }
+    if (val) {
+      val = val.trim()
+    }
+    parsed[key] = val
+  })
+
+  return parsed
+}
