c3b402e8f399ee83bdf36aa19dcbf02a95c2bc09
wangj <wangjun20101120@163.com>
2019-08-02 13:51:30 +0800

    多配置 以及多实例化

---
+++ src/core/xhr.ts
@@ -0,0 +1,68 @@
+import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
+import { createError } from '../helpers/error'
+
+export default function xhr(config: AxiosRequestConfig): AxiosPromise {
+
+  return new Promise((resolve, reject) => {
+
+    const {data = null, url, method = 'get', headers, responseType, timeout} = config
+
+    const request = new XMLHttpRequest()
+
+    if (responseType) {
+      request.responseType = responseType
+    }
+    if (timeout) {
+      request.timeout = timeout
+    }
+
+    request.open(method, url!, true)
+
+    request.onreadystatechange = function handlerLoad () {
+      if (request.readyState !== 4) {
+        return
+      }
+      if (request.status === 0) {
+        return
+      }
+      const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+      const responseData = request.responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: responseData,
+        status: request.status,
+        statusText: request.statusText,
+        headers: responseHeaders,
+        config,
+        request
+      }
+      handlerResponse(response)
+    }
+
+    request.onerror = function handlerError () {
+      reject(createError('network Error', config, null, request))
+    }
+
+    request.ontimeout = function handlerTimeout () {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+
+    Object.keys(headers).forEach(name => {
+      if (data === null && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+
+    request.send(data)
+
+    function handlerResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(createError(`Request failed with status code ${response.status}`, config, null, request, response))
+      }
+    }
+  })
+};
