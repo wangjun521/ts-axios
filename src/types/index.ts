608ac6cfea4328c829caa85f178db7ed07e5ff92
wangj <wangjun20101120@163.com>
2019-08-07 09:58:00 +0800

    取消请求功能

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,6 +11,7 @@ export interface AxiosRequestConfig {
   timeout?: number
   transformRequest?: AxiosTransFormer | AxiosTransFormer[]
   transformResponse?: AxiosTransFormer | AxiosTransFormer[]
+  cancelToken?: CancelToken
   [propName: string]: any
 }
 
@@ -70,6 +71,10 @@ export interface AxiosInstance extends Axios{
 
 export interface AxiosStatic extends AxiosInstance{
   create(config?: AxiosRequestConfig): AxiosInstance
+
+  CancelToken: CancelTokenStatic
+  Cancel: CancelStatic
+  isCancel: (value: any) => boolean
 }
 
 export interface AxiosInterceptorManager<T> {
@@ -88,6 +93,40 @@ export interface RejectedFn {
 
 export interface AxiosTransFormer {
   (data:any, headers?:any): any
+}
+
+export interface CancelToken {
+  promise: Promise<Cancel>
+  reason?: Cancel
+
+  throwIfRequested() : void
+}
+
+export interface Canceler {
+  (message?: string): void
+}
+
+export interface CancelExecutor {
+  (cancel: Canceler): void
+}
+
+export interface CancelTokenSource {
+  token: CancelToken
+  cancel: Canceler
+}
+
+export interface CancelTokenStatic {
+  new(executor: CancelExecutor): CancelToken
+
+  source(): CancelTokenSource
+}
+
+export interface Cancel {
+  message?: string
+}
+
+export interface CancelStatic {
+  new(message?: string): Cancel
 }
 
 
